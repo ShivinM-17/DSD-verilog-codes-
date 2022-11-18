017eb637a9305f77ae24f89449a11166270bfef2
ShubhamSharma2003 <117969915+ShubhamSharma2003@users.noreply.github.com>
2022-11-18 21:47:46 +0530

    Update t_ff_str.v

--- t flip flop/t_ff_str.v
+++ t flip flop/t_ff_str.v
@@ -1,7 +1,7 @@
 module t_ff_str(q,qb,t,clk);
 input t,clk;
 output q,qb;
-wire w1,w2,w3,w4,w5;
+wire w1,w2;
 
 nand l1(w1,t,clk,q);  //upper part of the circuit -- going for q
 nand l2(w2,t,clk,qb); //lower part of the circuit -- going for q'
