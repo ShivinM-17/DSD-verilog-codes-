ed5285ffaca74967068acac57db1cfa4a3edb1f9
ShivinM-17 <94225434+ShivinM-17@users.noreply.github.com>
2022-11-18 09:47:29 +0530

    Update decoder_3_8_beh.v

--- decoder_3_8/decoder_3_8_beh.v
+++ decoder_3_8/decoder_3_8_beh.v
@@ -6,14 +6,14 @@ always @(a or b or c or en)
 begin
     if(en==1)
         case({a,b,c})
-            3'b000: begin y[0]=1'b0;end
-            3'b001: begin y[1]=1'b0;end
-            3'b010: begin y[2]=1'b0;end
-            3'b011: begin y[3]=1'b0;end
-            3'b100: begin y[4]=1'b0;end
-            3'b101: begin y[5]=1'b0;end
-            3'b110: begin y[6]=1'b0;end
-            3'b111: begin y[7]=1'b0;end
+            3'b000: begin y[0]=1'b1;end
+            3'b001: begin y[1]=1'b1;end
+            3'b010: begin y[2]=1'b1;end
+            3'b011: begin y[3]=1'b1;end
+            3'b100: begin y[4]=1'b1;end
+            3'b101: begin y[5]=1'b1;end
+            3'b110: begin y[6]=1'b1;end
+            3'b111: begin y[7]=1'b1;end
         endcase
     else
          y=8'b0;
